d84a0a448043efa6428b708c67631e7041dbdff2
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-12 01:07:01 -0400

    Revert "added admin auth"
    This reverts commit 15825317e25545c3798e3e3b9e6bc809e91099d9.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], 
-  password: ENV['ADMIN_PASSWORD'], only: :index
 
   def index
     @products = Product.order(id: :desc).all
